6523105c11d2a715b40398d2f3fe8ae3a2fd0b85
Duda Koehler <101596709+DudaKoehler-dev@users.noreply.github.com>
2024-02-29 15:00:46 -0300

    Avanços SQLite

---
+++ MauiAppListaDeCompras/Models/Produto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+
+
+namespace MauiAppListaDeCompras.Models
+{
+    public class Produto
+    {
+        [AutoIncrement, PrimaryKey] 
+        public int Id { get; set; }
+        public string Descricao { get; set; }
+        public double Quantidade { get; set; }
+        public double Preco {  get; set; }
+
+        public double Total { get; set; }
+        
+    }
+}
